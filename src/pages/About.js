2cba509b4ee41b9ae39935b88285edfd25a0f29c
UthpalaPitawela <uthpala.isiru@gmail.com>
2020-09-12 08:11:15 +0530

    #7 About Page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,18 @@
 import React from "react";
 
 export default function About() {
-  return <h1>hello from about page</h1>;
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        I'm baby health goth blue bottle sustainable truffaut. Cardigan bitters
+        woke trust fund try-hard affogato deep v everyday carry kogi
+        chicharrones. Organic umami vinyl cred messenger bag. Live-edge lomo
+        tousled whatever pok pok selvage tote bag VHS ugh lo-fi fashion axe
+        mixtape kale chips franzen single-origin coffee. Gochujang celiac
+        iceland coloring book sriracha la croix slow-carb tacos blue bottle
+        readymade YOLO microdosing.
+      </p>
+    </section>
+  );
 }
