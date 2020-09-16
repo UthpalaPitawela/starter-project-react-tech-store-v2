cc5d7fdf22aa6e7cc79eb30e294e64c3749b6df6
UthpalaPitawela <uthpala.isiru@gmail.com>
2020-09-16 22:34:01 +0530

    #19 Cart page

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -12,5 +12,22 @@ export default function Cart() {
   if (cart.length === 0) {
     return <EmptyCart />;
   }
-  return <h1>hello from cart page</h1>;
+  return (
+    <section className="cart-items section">
+      <h2>your cart</h2>
+      {cart.map((item) => {
+        return <CartItem key={item.id} {...item} />;
+      })}
+      <h2>total: ${total}</h2>
+      {user ? (
+        <Link to="/checkout" className="btn btn-primary btn-block">
+          checkout
+        </Link>
+      ) : (
+        <Link to="/login" className="btn btn-primary btn-block">
+          login
+        </Link>
+      )}
+    </section>
+  );
 }
