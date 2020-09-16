9eee465a84ef3ba29af2b75bcfac674639a77464
UthpalaPitawela <uthpala.isiru@gmail.com>
2020-09-16 22:20:14 +0530

    #18 Empty Cart

--- src/context/cart.js
+++ src/context/cart.js
@@ -4,11 +4,11 @@ import localCart from "../utils/localCart";
 const CartContext = React.createContext();
 
 function CartProvider({ children }) {
-  const [cart, setCart] = React.useState(localCart);
+  const [cart, setCart] = React.useState([]);
   const [total, setTotal] = React.useState(0);
   const [cartItems, setCartItems] = React.useState(0);
   return (
-    <CartContext.Provider value={(cart, total, cartItems)}>
+    <CartContext.Provider value={{ cart, total, cartItems }}>
       {children}
     </CartContext.Provider>
   );
